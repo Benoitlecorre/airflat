be55caff0332bca6b30e46e97b7c97b4b5889547
Emmanuel Louisy-Gabriel <emmanuel@skycitizen.fr>
2014-05-21 10:04:03 +0200

    fixed photo table

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -35,6 +35,7 @@ class FlatsController < ApplicationController
 
   def update
     @flat.update!(flat_params)
+    @flat.photos.create(photo_params)
     flash[:info] = "You have updated successfully the #{@flat.title}"
     redirect_to flat_path(@flat)
   end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,6 +4,6 @@ class Photo < ActiveRecord::Base
 
   styles: { medium: "300x300>", thumb: "100x100>" }
 
-  #validates_attachment_content_type :file, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :file, :content_type => /\Aimage\/.*\Z/
 
 end
