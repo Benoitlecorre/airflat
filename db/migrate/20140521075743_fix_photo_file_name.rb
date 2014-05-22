79136a8844e62f8b3667fb326c62fe1f6a0a9aeb
MARCO <lefollmarc@hotmail.fr>
2014-05-22 10:34:25 +0200

    fix migration

--- db/migrate/20140521075743_fix_photo_file_name.rb
+++ db/migrate/20140521075743_fix_photo_file_name.rb
@@ -1,6 +1,5 @@
 class FixPhotoFileName < ActiveRecord::Migration
   def change
-    drop_table :photos
     create_table :photos do |t|
       t.references :flat, index: true
       t.attachment :file
