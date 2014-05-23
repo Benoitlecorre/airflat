9c850242fe0e32f94d508e78b1f2ed4552cd1780
Emmanuel Louisy-Gabriel <emmanuel@skycitizen.fr>
2014-05-23 11:21:25 +0200

    added geoloc feature on show view with geocoder

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,5 +1,6 @@
 class Flat < ActiveRecord::Base
   has_many :photos
   belongs_to :user
-
+  geocoded_by :address   # can also be an IP address
+  after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
 end
